bee0b1811a4b49b52764ca910a559e1440ddbd29
ikFe11 <rekituber2011@gmail.com>
2025-08-01 12:38:00 +0200

    new branch

--- Contents.swift
+++ Contents.swift
@@ -1,17 +1,20 @@
 import UIKit
 
 var laberynth = [
-    [3, 3, 3, 3, 3, 3, 3],
-    [3, 0, 0, 0, 0, 0, 3],
-    [3, 0, 3, 3, 3, 3, 3],
-    [3, 0, 3, 0, 0, 0, 3],
-    [3, 0, 3, 0, 3, 0, 3],
-    [3, 0, 0, 0, 3, 0, 3],
-    [3, 3, 3, 3, 3, 3, 3]
+    [8, 8, 8, 8, 8, 8, 8, 8, 8],
+    [8, 0, 0, 0, 0, 0, 0, 8, 8],
+    [8, 8, 8, 8, 0, 8, 0, 8, 8],
+    [8, 0, 0, 0, 0, 8, 0, 0, 8],
+    [8, 0, 8, 0, 8, 8, 8, 8, 8],
+    [8, 0, 8, 0, 8, 0, 0, 0, 8],
+    [8, 8, 8, 0, 8, 0, 8, 0, 8],
+    [8, 0, 0, 0, 0, 0, 8, 0, 8],
+    [8, 8, 8, 8, 8, 8, 8, 8, 8]
 ]
-//He puesto paredes para el algoritmo
+
+
 let start = (row: 1, col: 1)
-let end = (row: 5, col: 5)
+let end = (row: 7, col: 7)
 var yourPos = start
 typealias Position = (row: Int, col: Int)
 
@@ -29,7 +32,6 @@ func esValida( pos: Position,  mapa: [[Int]]) -> Bool {
 
 func autoMarker(pos : inout Position, mapa : inout [[Int]])-> [[Int]]{
     pos = start
-    mapa[pos.row][pos.col] = 1
     var CheckMark = ""
     var checked = false
     var requirement = 0
@@ -39,79 +41,76 @@ func autoMarker(pos : inout Position, mapa : inout [[Int]])-> [[Int]]{
         
 
         if mapa[pos.row][pos.col - 1] == requirement{
+            mapa[pos.row][pos.col] += 1
             pos.col -= 1
             mapa[pos.row][pos.col] += 1
             print (mapa)
-            print (requirement)
             requirement = 0
             
         }
         else if mapa[pos.row][pos.col + 1] == requirement{
+            mapa[pos.row][pos.col] += 1
             pos.col += 1
             mapa[pos.row][pos.col] += 1
             print (mapa)
-            print (requirement)
             requirement = 0
             
         }
         else if mapa[pos.row - 1][pos.col] == requirement{
+            mapa[pos.row][pos.col] += 1
             pos.row -= 1
             mapa[pos.row][pos.col] += 1
             print (mapa)
-            print (requirement)
             requirement = 0
             
         }
          else if mapa[pos.row + 1][pos.col] == requirement{
+             mapa[pos.row][pos.col] += 1
              pos.row += 1
              mapa[pos.row][pos.col] += 1
              print (mapa)
-             print (requirement)
              requirement = 0
              
         }
         else {
             requirement += 1
         }
-         
-         
-        
-        
-        
-        
         
     }
+
     return mapa
 }
 
 
 func printMap (mapa : [[Int]],  _ n0 : String,  _ n1 : String,  _ n2 : String ){
     var MAP: [[String]] = [
-        ["", "", "", "", "", "", ""],
-        ["", "", "", "", "", "", ""],
-        ["", "", "", "", "", "", ""],
-        ["", "", "", "", "", "", ""],
-        ["", "", "", "", "", "", ""],
-        ["", "", "", "", "", "", ""],
-        ["", "", "", "", "", "", ""]
+        ["", "", "", "", "", "", "", "", ""],
+        ["", "", "", "", "", "", "", "", ""],
+        ["", "", "", "", "", "", "", "", ""],
+        ["", "", "", "", "", "", "", "", ""],
+        ["", "", "", "", "", "", "", "", ""],
+        ["", "", "", "", "", "", "", "", ""],
+        ["", "", "", "", "", "", "", "", ""],
+        ["", "", "", "", "", "", "", "", ""],
+        ["", "", "", "", "", "", "", "", ""]
     ]
     var x = 0
     var y = 0
     var countForX = 0
     
-    for row in 1...7 {
-        for col in 1...7 {
+    for row in 1...9 {
+        for col in 1...9 {
             
             
             if mapa[x][y] == 0{
                 MAP [x][y] = n0
                 
             }
-            else if mapa[x][y] == 1 || mapa[x][y] == 2{
+            else if mapa[x][y] == 1 || mapa[x][y] == 2 || mapa[x][y] == 3 || mapa[x][y] == 4 || mapa[x][y] == 5 || mapa[x][y] == 6 || mapa[x][y] == 7{
                 MAP [x][y] = n1
                 
             }
-            else if mapa[x][y] == 3{
+            else if mapa[x][y] == 8{
                 MAP [x][y] = n2
                 
             }
@@ -120,7 +119,7 @@ func printMap (mapa : [[Int]],  _ n0 : String,  _ n1 : String,  _ n2 : String ){
             }
             
             
-            if countForX == 6{
+            if countForX == 8{
                 y = 0
                 countForX = 0
                 x += 1
